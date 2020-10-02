adc2e247978306335f72574af122b778a44ed0e4
Rickard Pedersen <pedersen.rickard@outlook.com>
2020-10-02 22:57:50 +0200

    final build

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
 	outputDir: '../public',
+	chainWebpack: config => {
+		config
+			.plugin('html')
+			.tap(args => {
+				args[0].title = 'SWAPI App'
+				return args
+			})
+	}
 }
